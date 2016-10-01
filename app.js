b2af597c686978d6229a6644c01b4309f33831ab
Javier Pelozo <javpelozo@gmail.com>
2016-09-30 23:28:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,15 +25,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', routes);
 */
 
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-*/
-
-// error handlers
 
 // development error handler
 // will print stacktrace
